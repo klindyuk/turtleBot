88eb7e43d4641c9907127bdec2e34858401ed07e
klindyuk <klindyuk@gmail.com>
2021-04-07 11:06:02 +0300

    Добавлено приветственное сообщение

--- bot.py
+++ bot.py
@@ -6,12 +6,24 @@ import turtle
 
 MAX_ARG = 50
 
+helloMsg = f'''Привет. Это бот-черепашка. Используйте эти команды для рисования:
+опустить хвост - оставлять след при перемещении
+поднять хвост - не оставлять след
+вперед х - переместиться вперед на х шагов
+назад х - переместиться назад на х шагов
+влево х - повернуть влево на х градусов
+вправо х - повернуть вправо на х градусов.
+Имейте в виду, что перемещение за одну команду ограничено {MAX_ARG} шагами'''
+
 bot = telebot.TeleBot(api.token, threaded=False)
+@bot.message_handler(commands=['start'])
+def hello(message):
+    bot.send_message(message.chat.id, helloMsg)
 
 @bot.message_handler(content_types=['text'])
 def anyMessage(message):
     command = message.text.lower().strip()
-    print(command)
+    print(f'{message.from_user.first_name}: {command}')
     if command == 'reset' and message.from_user.id == 379884080:
         turtle.reset()
     elif command == 'поднять хвост':
